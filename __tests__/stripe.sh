2bc522faff98a13dcd92f0897990135bf52562c1
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:22 -0300

    improve

--- __tests__/stripe.sh
+++ __tests__/stripe.sh
@@ -1,1 +1,9 @@
-stripe listen --forward-to localhost:3000/api/webhooks/stripe
+#!/bin/bash
+
+# Check if stripe command is installed
+if ! [ -x "$(command -v stripe)" ]; then
+  echo 'Error: stripe command not found' >&2
+  exit 1
+fi
+
+# Check if port 300
