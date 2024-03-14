0acf0207cab9428b8d813eb9685dd04a4182dc44
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:41 -0300

    improve

--- src/components/types/index.ts
+++ src/components/types/index.ts
@@ -1,1 +1,27 @@
-export * from "./propTypes";
+import type * as PropTypes from "./propTypes";
+
+export { PropTypes };
+
+
+declare module "./propTypes" {
+  export * from "./propTypes";
+}
+
+declare module "*.props" {
+  const value: any;
+  export default value;
+}
+
+
+export type TextPropTypes = {
+  children?: string | JSX.Element | JSX.Element[];
+  className?: string;
+};
+
+export type ButtonPropTypes = {
+  children?: string | JSX.Element | JSX.Element[];
+  className?: string;
+  onClick?: () => void;
+};
+
+// Add more prop types as needed
