5f4f72f7192156fa261ac9d75f58d59a20bd79e4
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:54 -0300

    improve

--- src/server/api/routers/account.ts
+++ src/server/api/routers/account.ts
@@ -1,22 +1,36 @@
 import { z } from "zod";
-
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import Stripe from "stripe";
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../db";
 import { getCustomerId } from "../../../utils/stripe-utils";
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-const stripe = new Stripe(env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2022-11-15",
+Stripe.setAppInfo({
+  name: "My App",
+  version: "1.0.0",
 });
+Stripe.setApiVersion("2022-11-15");
+
+const stripe = new Stripe(env.STRIPE_SECRET_KEY ?? "", {});
 
 export const accountRouter = createTRPCRouter({
   subscribe: protectedProcedure.mutation(async ({ ctx }) => {
-    const user = await prisma.user.findUniqueOrThrow({
+    const { id } = ctx.session?.user || {};
+
+    const userSchema = z.object({
+      id: z.string(),
+      email: z.string().optional(),
+      customerId: z.string().optional(),
+    });
+
+    const user = userSchema.parse(await prisma.user.findUniqueOrThrow({
       where: {
-        id: ctx.session?.user?.id,
+        id,
       },
+    }));
+
+    const customer = await stripe.customers.create({
+      email: user.email,
     });
 
     const checkoutSession = await stripe.checkout.sessions.create({
@@ -25,36 +39,13 @@ export const accountRouter = createTRPCRouter({
       mode: "subscription",
       line_items: [
         {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
           price: env.STRIPE_SUBSCRIPTION_PRICE_ID ?? "",
           quantity: 1,
         },
       ],
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      customer: user.customerId ?? undefined,
-      customer_email: user.email ?? undefined,
-      client_reference_id: ctx.session?.user?.id,
+      customer: customer.id,
+      client_reference_id: id,
       metadata: {
-        userId: ctx.session?.user?.id,
+        userId: id,
       },
     });
-
-    return checkoutSession.url;
-  }),
-  manage: protectedProcedure.mutation(async ({ ctx }) => {
-    if (!ctx.session?.user?.subscriptionId) {
-      return null;
-    }
-
-    const sub = await stripe.subscriptions.retrieve(
-      ctx.session?.user?.subscriptionId
-    );
-
-    const session = await stripe.billingPortal.sessions.create({
-      customer: getCustomerId(sub.customer),
-      return_url: env.NEXTAUTH_URL,
-    });
-
-    return session.url;
-  }),
-});
