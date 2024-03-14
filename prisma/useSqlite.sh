c39f88a3ebd9895e744acaa443b7997c25809df9
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:28 -0300

    improve

--- prisma/useSqlite.sh
+++ prisma/useSqlite.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-cd "$(dirname "$0")"
 
-sed -ie 's/mysql/sqlite/g' schema.prisma
-sed -ie 's/postgres/sqlite/g' schema.prisma
-sed -ie 's/@db.Text//' schema.prisma
+# Change to the directory where this script is located
+cd "$(dirname "$0")" || exit
+
+# Use a function to perform the sed replacements,
+# so we don't have to repeat the same command for each replacement.
+replace_in_file() {
+  local file=$1
+  local search=$2
+  local replace=$3
+  shift 3 || exit
+  sed -i "$@" "s/${search}/${replace}/g" "${file}"
+}
+
+# Perform the replacements
+replace_in_file schema.prisma 'mysql' 'sqlite'
+replace_in_file schema.prisma 'postgres' 'sqlite'
+replace_in_file schema.prisma '@db.Text' ''
+
