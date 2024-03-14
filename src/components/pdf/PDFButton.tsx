d46a3718fb6db3b8927a0ca6842510fe8972cfa3
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:40 -0300

    improve

--- src/components/pdf/PDFButton.tsx
+++ src/components/pdf/PDFButton.tsx
@@ -1,30 +1,32 @@
 import WindowButton from "../WindowButton";
 import { FaSave } from "react-icons/fa";
 import { pdf } from "@react-pdf/renderer";
-import React from "react";
+import React, { type FC } from "react";
 import type { Message } from "../ChatWindow";
 import MyDocument from "./MyDocument";
 
-const PDFButton = ({ messages }: { messages: Message[] }) => {
+const PDFButton: FC<{ messages: Message[] }> = ({ messages }) => {
   const content = getContent(messages);
 
   const downloadPDF = async () => {
-    const blob = await pdf(<MyDocument content={content} />).toBlob();
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = "my-document.pdf";
-    link.click();
-    URL.revokeObjectURL(url);
+    try {
+      const blob = await pdf(<MyDocument content={content} />).toBlob();
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = "my-document.pdf";
+      link.click();
+      URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <>
       <WindowButton
         delay={0.8}
-        onClick={() => {
-          downloadPDF().catch(console.error);
-        }}
+        onClick={downloadPDF}
         icon={<FaSave size={12} />}
         text={"Save"}
       />
@@ -33,17 +35,9 @@ const PDFButton = ({ messages }: { messages: Message[] }) => {
 };
 
 const getContent = (messages: Message[]): string => {
-  // Note "Thinking" messages have no `value` so they show up as new lines
   return messages
-    .map((message) => {
-      if (message.type == "goal") {
-        return `Goal: ${message.value}`;
-      }
-      if (message.type == "task") {
-        return `Adding Task: ${message.value}`;
-      }
-      return message.value;
-    })
+    .filter(message => message.type === "goal" || message.type === "task")
+    .map(message => `${message.type}: ${message.value}`)
     .join("\n");
 };
 
