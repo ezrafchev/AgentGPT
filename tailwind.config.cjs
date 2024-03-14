f9155c6aa955a14851bae3cc26d2cec2911f46d6
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:49 -0300

    improve

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,27 @@
-/** @type {import("tailwindcss").Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    // Include any additional directories that contain your HTML templates or other files that use Tailwind classes
+    './public/index.html',
+  ],
   theme: {
     screens: {
-      "xs": "300px",
-
-      "sm-h": { "raw": "(min-height: 700px)" },
-      "md-h": { "raw": "(min-height: 800px)" },
-      "lg-h": { "raw": "(min-height: 1000px)" },
-
-      ...defaultTheme.screens
+      // Use the default Tailwind screen sizes, with the addition of custom min-height screens
+      xs: '300px',
+      ...defaultTheme.screens,
+      smH: { 'raw': '(min-height: 700px)' },
+      mdH: { 'raw': '(min-height: 800px)' },
+      lgH: { 'raw': '(min-height: 1000px)' },
     },
     extend: {
       boxShadow: {
-        "3xl": "0 40px 70px -15px rgba(0, 0, 0, 0.40)" // Customize the shadow value according to your preferences.
-      }
-    }
+        '3xl': '0 40px 70px -15px rgba(0, 0, 0, 0.40)',
+        // Add any additional custom shadows or other styles here
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
