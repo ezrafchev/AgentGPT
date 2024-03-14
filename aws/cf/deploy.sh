0359a0cff564eafb291724e66bdb237a5b140f5f
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:53 -0300

    improve

--- aws/cf/deploy.sh
+++ aws/cf/deploy.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-cd "$(dirname "$0")"
 
-aws cloudformation create-stack --stack-name agent \
-  --template-body file:///$PWD/agent.cf.json
+# Check for AWS CLI
+if ! command -v aws &> /dev/null
+then
+    echo "AWS CLI not found. Please install it and try again."
+    exit 1
+fi
+
+# Check for agent.cf.json file
+if [ ! -f "$(pwd)/agent.cf.json" ]
+then
+    echo "agent.cf.json file not found. Please make sure it exists and try again."
+    exit 1
+fi
+
+set -e
+
+aws cloudformation create-stack \
+--stack-name agent \
+--template-body file://"$(pwd)"/agent.cf.json \
+--capabilities CAPABILITY_NAMED_IAM \
+--region us-west-2 \
+--timeout-in-minutes 10 \
+--profile my-aws-profile
