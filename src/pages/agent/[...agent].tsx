a99e28131e8ef7b9e38ed4a0b421e98cfcc30c24
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:29 -0300

    improve

--- src/pages/agent/[...agent].tsx
+++ src/pages/agent/[...agent].tsx
@@ -1,21 +1,30 @@
 import { type NextPage } from "next";
 import DefaultLayout from "../../layout/default";
-import Button from "../../components/Button";
+import Button, { type ButtonProps } from "../../components/Button";
 
 import React from "react";
 import { useRouter } from "next/router";
 
-const Home: NextPage = () => {
+type HomeProps = {};
+
+const Home: NextPage<HomeProps> = () => {
   const router = useRouter();
+
+  const handleBackClick: ButtonProps["onClick"] = () => {
+    router.push("/");
+  };
 
   return (
     <DefaultLayout className="flex flex-col items-center justify-center gap-4">
       <h1 className="text-center text-4xl font-bold text-[#C0C0C0]">
         Coming Soon!{" "}
       </h1>
-      <Button onClick={() => void router.push("/")}>Back</Button>
+      <Button data-testid="back-button" title="Back" onClick={handleBackClick}>
+        Back
+      </Button>
     </DefaultLayout>
   );
 };
 
 export default Home;
+
