a86591e64f774e924661ec20bee7b7e95e0af218
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:42 -0300

    improve

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,22 @@
-import { type AppType } from "next/app";
+import type { AppProps, AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
 import { api } from "../utils/api";
-
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 
-const MyApp: AppType<{ session: Session | null }> = ({
+type PageProps = AppProps & {
+  session: Session | null;
+};
+
+const MyApp: AppType<PageProps> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
-      <Analytics />
+      {session && <Analytics />}
     </SessionProvider>
   );
 };
