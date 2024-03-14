5d99e3e3810c11dc2c3f1c9d3bbb7628a19b16c2
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:24 -0300

    improve

--- jest.config.cjs
+++ jest.config.cjs
@@ -13,7 +13,12 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  transform: {
+    // Handle Next.js specific files
+    ".+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/next-transpile-modules/index.js",
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
+
