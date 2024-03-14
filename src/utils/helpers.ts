3ad09733b479b3b3a5fd5ed6aee0bf47fb3bbdaf
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:46 -0300

    improve

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,20 +1,23 @@
-type Constructor<T> = new (...args: unknown[]) => T;
+type Constructor<T = any> = new (...args: any[]) => T;
+
+type TypeGuard<T> = (value: unknown) => value is T;
 
 /* Check whether array is of the specified type */
 const isArrayOfType = <T>(
   arr: unknown[] | unknown,
   type: Constructor<T> | string
 ): arr is T[] => {
-  return (
-    Array.isArray(arr) &&
-    arr.every((item): item is T => {
-      if (typeof type === "string") {
-        return typeof item === type;
-      } else {
-        return item instanceof type;
-      }
-    })
-  );
+  if (!Array.isArray(arr)) return false;
+
+  const typeGuard: TypeGuard<T> = (value: unknown): value is T => {
+    if (typeof type === "string") {
+      return typeof value === type;
+    } else {
+      return value instanceof type;
+    }
+  };
+
+  return arr.every(typeGuard);
 };
 
 export default isArrayOfType;
