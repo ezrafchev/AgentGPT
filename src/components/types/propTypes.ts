e07565230bd9f13962c1932729af4d64f1be5fff
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:47 -0300

    improve

--- src/components/types/propTypes.ts
+++ src/components/types/propTypes.ts
@@ -1,15 +1,25 @@
-export type toolTipProperties = {
+import React, { createContext, Dispatch, SetStateAction } from 'react';
+
+export type TooltipProperties = {
   message?: string;
   disabled?: boolean;
 };
 
-export type reactModelStates = {
-  customApiKey: string;
-  setCustomApiKey: (key: string) => void;
-  customModelName: string;
-  setCustomModelName: (key: string) => void;
-  customTemperature: number;
-  setCustomTemperature: (temperature: number) => void;
-  customMaxLoops: number;
-  setCustomMaxLoops: (numberOfLoops: number) => void;
+type ModelState<T> = {
+  value: T;
+  setValue: Dispatch<SetStateAction<T>>;
 };
+
+export type ReactModelStates = {
+  customApiKey: ModelState<string>;
+  customModelName: ModelState<string>;
+  customTemperature: ModelState<number>;
+  customMaxLoops: ModelState<number>;
+};
+
+export const ModelContext = createContext<ReactModelStates>({
+  customApiKey: { value: '', setValue: () => {} },
+  customModelName: { value: '', setValue: () => {} },
+  customTemperature: { value: 1, setValue: () => {} },
+  customMaxLoops: { value: 100, setValue: () => {} },
+});
