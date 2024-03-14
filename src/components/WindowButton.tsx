22e94556ff9beaa8261e043d323ae8c007377e96
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:41 -0300

    improve

--- src/components/WindowButton.tsx
+++ src/components/WindowButton.tsx
@@ -3,19 +3,19 @@ import React from "react";
 
 type WindowButtonProps = {
   delay: number;
-  onClick: () => void;
-  icon: React.ReactNode;
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  icon: React.ReactElement<any>;
   text: string;
 };
 
-const WindowButton = ({ delay, onClick, icon, text }: WindowButtonProps) => {
+const WindowButton: React.FC<WindowButtonProps> = ({ delay, onClick, icon: Icon, text }) => {
   return (
-    <PopIn delay={delay}>
+    <PopIn key={text} delay={delay}>
       <div
         className="mr-1 flex cursor-pointer items-center gap-2 rounded-full border-2 border-white/30 p-1 px-2 text-xs hover:bg-white/10"
         onClick={onClick}
       >
-        {icon}
+        <Icon alt={`${text} icon`} />
         <p className="font-mono">{text}</p>
       </div>
     </PopIn>
