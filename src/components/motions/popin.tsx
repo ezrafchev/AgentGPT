4e1abcd9ab1560a556cc06157c6161950b58d7bd
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:29 -0300

    improve

--- src/components/motions/popin.tsx
+++ src/components/motions/popin.tsx
@@ -1,18 +1,21 @@
-import { motion } from "framer-motion";
-import type { PropsWithChildren } from "react";
+import { motion, Variants } from "framer-motion";
+import type { PropsWithChildren, ReactNode } from "react";
 
-interface MotionProps extends PropsWithChildren {
+interface PopInProps extends PropsWithChildren {
   className?: string;
   delay?: number;
+  initialScale?: number;
+  finalScale?: number;
 }
 
-const PopIn = (props: MotionProps) => (
-  <motion.div
-    initial={{ scale: 0 }}
-    animate={{ scale: 1 }}
-    transition={{ duration: 0.5, type: "spring", delay: props.delay ?? 0 }}
-    {...props}
-  >
+const popInVariants: Variants = {
+  initial: { scale: (props: PopInProps) => props.initialScale || 0 },
+  animate: { scale: (props: PopInProps) => props.finalScale || 1 },
+  transition: { duration: 0.5, type: "spring", delay: (props: PopInProps) => props.delay || 0 },
+};
+
+const PopIn = (props: PopInProps) => (
+  <motion.div variants={popInVariants} custom={props} {...props}>
     {props.children}
   </motion.div>
 );
