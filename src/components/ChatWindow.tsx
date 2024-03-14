32a295ebd69cae97ec459f015a80468913633051
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:38 -0300

    improve

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -1,17 +1,7 @@
-import type { ReactNode } from "react";
 import React, { useEffect, useRef, useState } from "react";
-import {
-  FaBrain,
-  FaClipboard,
-  FaListAlt,
-  FaPlayCircle,
-  FaStar,
-  FaCopy,
-  FaImage,
-} from "react-icons/fa";
 import PopIn from "./motions/popin";
 import Expand from "./motions/expand";
-import * as htmlToImage from "html-to-image";
+import htmlToImage from "html-to-image";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
@@ -19,47 +9,41 @@ import "highlight.js/styles/github-dark.css";
 import Button from "./Button";
 import { useRouter } from "next/router";
 import WindowButton from "./WindowButton";
-import PDFButton from "./pdf/PDFButton";
 import FadeIn from "./motions/FadeIn";
+import Message from "./Message";
 
-interface ChatWindowProps extends HeaderProps {
-  children?: ReactNode;
-  className?: string;
+interface ChatWindowProps {
   messages: Message[];
+  children?: React.ReactNode;
+  className?: string;
+  title: string;
   showDonation: boolean;
 }
 
 const messageListId = "chat-window-message-list";
 
-const ChatWindow = ({
+const ChatWindow: React.FC<ChatWindowProps> = ({
   messages,
   children,
   className,
   title,
   showDonation,
-}: ChatWindowProps) => {
+}) => {
   const [hasUserScrolled, setHasUserScrolled] = useState(false);
   const scrollRef = useRef<HTMLDivElement>(null);
 
   const handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
     const { scrollTop, scrollHeight, clientHeight } = event.currentTarget;
-
-    // Use has scrolled if we have scrolled up at all from the bottom
-    if (scrollTop < scrollHeight - clientHeight - 10) {
-      setHasUserScrolled(true);
-    } else {
-      setHasUserScrolled(false);
-    }
+    setHasUserScrolled(scrollTop < scrollHeight - clientHeight - 10);
   };
 
   useEffect(() => {
-    // Scroll to bottom on re-renders
-    if (scrollRef && scrollRef.current) {
+    if (scrollRef.current) {
       if (!hasUserScrolled) {
         scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
       }
     }
-  });
+  }, [hasUserScrolled]);
 
   return (
     <div
@@ -115,43 +99,31 @@ const ChatWindow = ({
 };
 
 interface HeaderProps {
-  title?: string | ReactNode;
+  title: string;
   messages: Message[];
 }
 
-const MacWindowHeader = (props: HeaderProps) => {
+const MacWindowHeader: React.FC<HeaderProps> = (props) => {
   const saveElementAsImage = (elementId: string) => {
     const element = document.getElementById(elementId);
-    if (!element) {
-      return;
+    if (element) {
+      htmlToImage
+        .toJpeg(element)
+        .then((dataUrl) => {
+          const link = document.createElement("a");
+          link.href = dataUrl;
+          link.download = "agent-gpt-output.png";
+          link.click();
+        })
+        .catch(console.error);
     }
-
-    htmlToImage
-      .toJpeg(element, {
-        height: element.scrollHeight,
-        style: {
-          overflowY: "visible",
-          maxHeight: "none",
-          border: "none",
-        },
-      })
-      .then((dataUrl) => {
-        const link = document.createElement("a");
-        link.href = dataUrl;
-        link.download = "agent-gpt-output.png";
-        link.click();
-      })
-      .catch(console.error);
   };
 
   const copyElementText = (elementId: string) => {
     const element = document.getElementById(elementId);
-    if (!element) {
-      return;
+    if (element) {
+      navigator.clipboard.writeText(element.innerText);
     }
-
-    const text = element.innerText;
-    void navigator.clipboard.writeText(text);
   };
 
   return (
@@ -172,149 +144,4 @@ const MacWindowHeader = (props: HeaderProps) => {
         delay={0.7}
         onClick={(): void => saveElementAsImage(messageListId)}
         icon={<FaImage size={12} />}
-        text={"Image"}
-      />
-
-      <WindowButton
-        delay={0.8}
-        onClick={(): void => copyElementText(messageListId)}
-        icon={<FaClipboard size={12} />}
-        text={"Copy"}
-      />
-      <PDFButton messages={props.messages} />
-    </div>
-  );
-};
-const ChatMessage = ({ message }: { message: Message }) => {
-  const [showCopy, setShowCopy] = useState(false);
-  const [copied, setCopied] = useState(false);
-  const handleCopyClick = () => {
-    void navigator.clipboard.writeText(message.value);
-    setCopied(true);
-  };
-
-  useEffect(() => {
-    let timeoutId: NodeJS.Timeout;
-    if (copied) {
-      timeoutId = setTimeout(() => {
-        setCopied(false);
-      }, 2000);
-    }
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, [copied]);
-
-  return (
-    <div
-      className="mx-2 my-1 rounded-lg border-[2px] border-white/10 bg-white/20 p-1 font-mono text-sm hover:border-[#1E88E5]/40 sm:mx-4 sm:p-3 sm:text-base"
-      onMouseEnter={() => setShowCopy(true)}
-      onMouseLeave={() => setShowCopy(false)}
-      onClick={handleCopyClick}
-    >
-      {message.type != "system" && (
-        // Avoid for system messages as they do not have an icon and will cause a weird space
-        <>
-          <div className="mr-2 inline-block h-[0.9em]">
-            {getMessageIcon(message)}
-          </div>
-          <span className="mr-2 font-bold">{getMessagePrefix(message)}</span>
-        </>
-      )}
-
-      {message.type == "thinking" && (
-        <span className="italic text-zinc-400">
-          (Restart if this takes more than 30 seconds)
-        </span>
-      )}
-
-      {message.type == "action" ? (
-        <div className="prose ml-2 max-w-none">
-          <ReactMarkdown
-            remarkPlugins={[remarkGfm]}
-            rehypePlugins={[rehypeHighlight]}
-          >
-            {message.value}
-          </ReactMarkdown>
-        </div>
-      ) : (
-        <span>{message.value}</span>
-      )}
-
-      <div className="relative">
-        {copied ? (
-          <span className="absolute bottom-0 right-0 rounded-full border-2 border-white/30 bg-zinc-800 p-1 px-2 text-gray-300">
-            Copied!
-          </span>
-        ) : (
-          <span
-            className={`absolute bottom-0 right-0 rounded-full border-2 border-white/30 bg-zinc-800 p-1 px-2 ${
-              showCopy ? "visible" : "hidden"
-            }`}
-          >
-            <FaCopy className="text-white-300 cursor-pointer" />
-          </span>
-        )}
-      </div>
-    </div>
-  );
-};
-
-const DonationMessage = () => {
-  const router = useRouter();
-
-  return (
-    <div className="mx-2 my-1 flex flex-col gap-2 rounded-lg border-[2px] border-white/10 bg-blue-500/20 p-1 font-mono hover:border-[#1E88E5]/40 sm:mx-4 sm:flex-row sm:p-3 sm:text-center sm:text-base">
-      <div className="max-w-none flex-grow">
-        💝️ Help support the advancement of AgentGPT. 💝
-        <br />
-        Please consider sponsoring the project on GitHub.
-      </div>
-      <div className="flex items-center justify-center">
-        <Button
-          className="sm:text m-0 rounded-full text-sm "
-          onClick={() =>
-            void router.push("https://github.com/sponsors/reworkd-admin")
-          }
-        >
-          Support now 🚀
-        </Button>
-      </div>
-    </div>
-  );
-};
-
-const getMessageIcon = (message: Message) => {
-  switch (message.type) {
-    case "goal":
-      return <FaStar className="text-yellow-300" />;
-    case "task":
-      return <FaListAlt className="text-gray-300" />;
-    case "thinking":
-      return <FaBrain className="mt-[0.1em] text-pink-400" />;
-    case "action":
-      return <FaPlayCircle className="text-green-500" />;
-  }
-};
-
-const getMessagePrefix = (message: Message) => {
-  switch (message.type) {
-    case "goal":
-      return "Embarking on a new goal:";
-    case "task":
-      return "Added task:";
-    case "thinking":
-      return "Thinking...";
-    case "action":
-      return message.info ? message.info : "Executing:";
-  }
-};
-
-export interface Message {
-  type: "goal" | "thinking" | "task" | "action" | "system";
-  info?: string;
-  value: string;
-}
-
-export default ChatWindow;
-export { ChatMessage };
+        text={
