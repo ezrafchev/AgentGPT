0bab9741709c66217c32fde51e25a287a87e9887
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:23 -0300

    improve

--- __tests__/extract-array.test.ts
+++ __tests__/extract-array.test.ts
@@ -1,18 +1,19 @@
 import { extractArray } from "../src/utils/chain";
+import { JSONString } from "../src/types/common";
 
 describe("Strings should be extracted from arrays correctly", () => {
-  it("simple", () => {
-    const modelResult = `
-  \`\`\`json
+  const modelResult: JSONString = `
 [
   "Research and implement natural language processing techniques to improve task creation accuracy.",
   "Develop a machine learning model to predict the most relevant tasks for users based on their past activity.",
   "Integrate with external tools and services to provide users with additional features such as task prioritization and scheduling."
 ]
-\`\`\`
 `;
-    expect(extractArray(modelResult).length).toBe(3);
-    expect(extractArray(modelResult).at(2)).toBe(
+
+  it("simple", () => {
+    const extractedArray = extractArray(modelResult);
+    expect(extractedArray.length).toBe(3);
+    expect(extractedArray[2]).toBe(
       "Integrate with external tools and services to provide users with additional features such as task prioritization and scheduling."
     );
   });
