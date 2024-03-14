8ff9c1f4f66923be46cae4d297587af80c17c3a2
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:51 -0300

    improve

--- src/server/api/routers/chain.ts
+++ src/server/api/routers/chain.ts
@@ -1,16 +1,20 @@
 import { z } from "zod";
-
 import { createTRPCRouter, publicProcedure } from "../trpc";
 import { startGoalAgent } from "../../../utils/chain";
 
 export const chainRouter = createTRPCRouter({
   startAgent: publicProcedure
-    .input(z.object({ prompt: z.string() }))
+    .input(z.object({ prompt: z.string().nonempty() }))
     .mutation(async ({ input }) => {
-      // const completion = (await startGoalAgent(input.prompt)) as {
-      //   text: string;
-      // };
-      //
-      // return { tasks: JSON.parse(completion.text) as string[] };
+      try {
+        const completion = await startGoalAgent(input.prompt);
+        return { tasks: completion.tasks };
+      } catch (error) {
+        console.error(error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "An error occurred while starting the agent.",
+        });
+      }
     }),
 });
