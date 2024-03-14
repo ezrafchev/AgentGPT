d195255998ba9e0f39884184c90383a02e92caec
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:22 -0300

    improve

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,7 @@
-/** @type {import("prettier").Config} */
+/**
+ * @type {import("prettier").Config}
+ * Configures Prettier to use the Tailwind CSS plugin for formatting.
+ */
 module.exports = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
