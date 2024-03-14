07b85445fb37a2a8e86daebad584e331d0493e7f
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:41 -0300

    improve

--- src/components/motions/FadeIn.tsx
+++ src/components/motions/FadeIn.tsx
@@ -1,21 +1,40 @@
-import { motion } from "framer-motion";
-import type { PropsWithChildren } from "react";
+import { motion, Variants } from "framer-motion";
+import type { PropsWithChildren, ReactNode } from "react";
 
-interface MotionProps extends PropsWithChildren {
+interface FadeInProps extends PropsWithChildren {
   className?: string;
   delay?: number;
+  initialPositionY?: number;
+  finalPositionY?: number;
 }
 
-const FadeIn = (props: MotionProps) => (
-  <motion.div
-    initial={{ opacity: 0, y: -30 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.5, type: "spring", delay: props.delay ?? 0.3 }}
-    {...props}
-  >
-    {props.children}
-  </motion.div>
-);
+const fadeInVariants: Variants = {
+  initial: { opacity: 0, y: -30 },
+  animate: { opacity: 1, y: 0 },
+};
 
-FadeIn.displayName = "FadeOut";
+const transitionVariants = {
+  type: "spring",
+  duration: 0.5,
+};
+
+const FadeIn = (props: FadeInProps) => {
+  const { children, delay = 0.3, initialPositionY = -30, finalPositionY = 0 } = props;
+
+  return (
+    <motion.div
+      variants={fadeInVariants}
+      initial="initial"
+      animate="animate"
+      transition={transitionVariants}
+      custom={delay}
+      className={props.className}
+      style={{ y: initialPositionY, transition: `all ${transitionVariants.duration}s ease` }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+FadeIn.displayName = "FadeIn";
 export default FadeIn;
