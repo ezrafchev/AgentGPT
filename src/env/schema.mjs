34b201da2ad15e5a9ce76a9affbbdae0842927bb
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:59 -0300

    improve

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -1,32 +1,29 @@
 // @ts-check
 import { z } from "zod";
 
-const requiredForProduction = () => process.env.NODE_ENV === "production"
-    ? z.string().min(1).trim()
-    : z.string().min(1).trim().optional()
+const requiredForProduction = () =>
+  process.env.NODE_ENV === "production"
+    ? z.string().min(1).trim().nonempty()
+    : z.string().min(1).trim().optional();
 
 function stringToBoolean() {
   return z.preprocess(
-      (str) => str === "true",
-      z.boolean(),
+    (str) => str.toLowerCase() === "true",
+    z.boolean()
   );
 }
 
-/**
- * Specify your server-side environment variables schema here.
- * This way you can ensure the app isn't built with invalid env vars.
- */
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET: requiredForProduction(),
-  NEXTAUTH_URL: z.preprocess(
-    // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-    // Since NextAuth.js automatically uses the VERCEL_URL if present.
-    (str) => process.env.VERCEL_URL ?? str,
-    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
-  ),
+  NEXTAUTH_URL: z
+    .preprocess(
+      (str) => process.env.VERCEL_URL ?? str,
+      process.env.VERCEL
+        ? z.string()
+        : z.string().url()
+    ),
   OPENAI_API_KEY: z.string(),
 
   GOOGLE_CLIENT_ID: requiredForProduction(),
@@ -41,12 +38,11 @@ export const serverSchema = z.object({
   STRIPE_SUBSCRIPTION_PRICE_ID: z.string().optional(),
 });
 
-/**
- * You can't destruct `process.env` as a regular object in the Next.js
- * middleware, so you have to do it manually here.
- * @type {{ [k in keyof z.input<typeof serverSchema>]: string | undefined }}
- */
-export const serverEnv = {
+type ServerEnv = {
+  [K in keyof z.input<typeof serverSchema>]: string | undefined;
+};
+
+export const serverEnv: ServerEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
@@ -56,7 +52,7 @@ export const serverEnv = {
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
   GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-  DISCORD_CLIENT_ID:  process.env.DISCORD_CLIENT_ID,
+  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
   DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
 
   STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
@@ -64,28 +60,20 @@ export const serverEnv = {
   STRIPE_SUBSCRIPTION_PRICE_ID: process.env.STRIPE_SUBSCRIPTION_PRICE_ID,
 };
 
-/**
- * Specify your client-side environment variables schema here.
- * This way you can ensure the app isn't built with invalid env vars.
- * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
- */
 export const clientSchema = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string(),
   NEXT_PUBLIC_VERCEL_ENV: z.enum(["production", "preview", "development"]),
   NEXT_PUBLIC_FF_AUTH_ENABLED: stringToBoolean(),
   NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().optional(),
   NEXT_PUBLIC_FF_SUB_ENABLED: stringToBoolean(),
 });
 
-/**
- * You can't destruct `process.env` as a regular object, so you have to do
- * it manually here. This is because Next.js evaluates this at build time,
- * and only used environment variables are included in the build.
- * @type {{ [k in keyof z.input<typeof clientSchema>]: string | undefined }}
- */
-export const clientEnv = {
+type ClientEnv = {
+  [K in keyof z.input<typeof clientSchema>]: string | undefined;
+};
+
+export const clientEnv: ClientEnv = {
   NEXT_PUBLIC_VERCEL_ENV: process.env.NEXT_PUBLIC_VERCEL_ENV ?? "development",
   NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
   NEXT_PUBLIC_FF_AUTH_ENABLED: process.env.NEXT_PUBLIC_FF_AUTH_ENABLED,
-  NEXT_PUBLIC_FF_SUB_ENABLED: process.env.NEXT_PUBLIC_FF_SUB_ENABLED
+  NEXT_PUBLIC_FF_SUB_ENABLED: process.env.NEXT_PUBLIC_FF_SUB_ENABLED,
 };
