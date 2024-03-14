7ab1a8b2aba60fd124823f8980c0ffb5126aa764
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:42 -0300

    improve

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,22 @@
+type ApiKey = string;
+type ModelName = string;
+type Temperature = number;
+type MaxLoops = number;
+
 export type ModelSettings = {
-  customApiKey: string;
-  customModelName: string;
-  customTemperature: number;
-  customMaxLoops: number;
+  customApiKey: ApiKey;
+  customModelName: ModelName;
+  customTemperature?: Temperature;
+  customMaxLoops?: MaxLoops;
 };
+
+const defaultModelSettings: ModelSettings = {
+  customApiKey: '',
+  customModelName: '',
+};
+
+export const getDefaultModelSettings = (): ModelSettings => ({
+  ...defaultModelSettings,
+  customTemperature: 1,
+  customMaxLoops: 100,
+});
