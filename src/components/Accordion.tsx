adb63883ce30f813ab9a7030adec30eb09ae53d1
Esdra Felipe <129979017+ezrafchev@users.noreply.github.com>
2024-03-13 21:58:30 -0300

    improve

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -11,17 +11,16 @@ const Accordion = ({ child, name }: AccordionProps) => {
     <AccordionPrimitive>
       {({ open }) => (
         <>
-          <AccordionPrimitive.Button className="border:black delay-50 mb-1 flex w-full items-center justify-between rounded-xl bg-[#4a4a4a] px-3 py-2 text-sm tracking-wider outline-0 transition-all placeholder:text-white/20 hover:border-[#1E88E5]/40 hover:bg-[#6b6b6b] focus:border-[#1E88E5] focus-visible:ring sm:py-3 md:mb-3 md:text-lg">
+          <AccordionPrimitive.Button
+            className="border border-black delay-50 mb-1 flex w-full items-center justify-between rounded-xl bg-[#4a4a4a] px-3 py-2 text-sm tracking-wider outline-0 transition-all placeholder:text-white/20 hover:border-[#1E88E5]/40 hover:bg-[#6b6b6b] focus:border-[#1E88E5] focus-visible:ring sm:py-3 md:mb-3 md:text-lg"
+            disabled={open}
+          >
             {name}
             <FaChevronDown
               className={`${open ? "rotate-180 transform" : ""} h-5 w-5`}
             />
           </AccordionPrimitive.Button>
-          <AccordionPrimitive.Panel>{child}</AccordionPrimitive.Panel>
+          <AccordionPrimitive.Panel key={name}>{child}</AccordionPrimitive.Panel>
         </>
       )}
-    </AccordionPrimitive>
-  );
-};
-
-export default Accordion;
+    
